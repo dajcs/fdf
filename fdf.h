b82c085b3aa4f8217dcc99d7419e2da8824a5e32
Attila Nemet <anemet@c2r7s5.42luxembourg.lu>
2025-07-15 09:15:06 +0200

    fdf.h, libft

---
+++ fdf.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anemet <anemet@student.42luxembourg.lu>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 09:00:39 by anemet            #+#    #+#             */
+/*   Updated: 2025/07/15 09:10:34 by anemet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include "libft/libft.h"
+# include "minilibx-linux/mlx.h"
+# include <fcntl.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# define WIN_WIDTH 800
+# define WIN_HEIGHT 600
+
+typedef struct s_point
+{
+	int		x;
+	int		y;
+	int		z;
+	int		color;
+}			t_point;
+
+typedef struct s_map
+{
+	int		width;
+	int		height;
+	int		**z_grid;
+}			t_map;
+
+typedef struct s_fdf
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_map	*map;
+}			t_fdf;
+
+int			read_map(const char *file, t_map *map);
+void		draw_map(t_fdf *fdf);
+
+#endif
